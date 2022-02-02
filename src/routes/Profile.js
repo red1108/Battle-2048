35e1c4fdfa3d2519b0fa182a57b20a1ac12e0886
Godthink_un <44489551+myeongjinshin@users.noreply.github.com>
2022-02-03 00:46:40 +0900

    amazon ads

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -26,26 +26,54 @@ const Profile = ({ userObj }) => {
     };
 
     useEffect(() => {
+        window.onresize = (e) => update_layout(e);
         getGameRecord();
         updateGA();
+        update_layout();
     }, []);
 
     return (
-        <center className="profile">
-            <button id="logoutBtn" onClick={onLogOutClick}>
-                Log Out
-            </button>
-            <div>
-                {records.map((record) => (
-                    <div key={record.id}>
-                        <h4 className="profile-history">
-                            {record.winner ? "Win" : "Lose"} {record.myScore}:{record.aiScore}
-                        </h4>
-                        <h4 className="profile-history">When: {record.endedAt}</h4>
-                    </div>
-                ))}
-            </div>
-        </center>
+        <div className="profile">
+            <center className="profile-info">
+                <button id="logoutBtn" onClick={onLogOutClick}>
+                    Log Out
+                </button>
+                <div>
+                    {records.map((record) => (
+                        <div key={record.id}>
+                            <h4 className="profile-history">
+                                {record.winner ? "Win" : "Lose"} {record.myScore}:{record.aiScore}
+                            </h4>
+                            <h4 className="profile-history">When: {record.endedAt}</h4>
+                        </div>
+                    ))}
+                </div>
+            </center>
+            <iframe
+                id="amazon-left-ads"
+                width="120"
+                height="240"
+                style={{
+                    marginWidth: 0 + "px",
+                    marginHeight: 0 + "px",
+                }}
+                scrolling="no"
+                frameBorder="0"
+                src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=2048b02-20&language=en_US&marketplace=amazon&region=US&placement=B07RLRWWPJ&asins=B07RLRWWPJ&linkId=ebcb1be7da04ed627ed8c807078d41b2&show_border=true&link_opens_in_new_window=true"
+            ></iframe>
+            <iframe
+                id="amazon-right-ads"
+                width="120"
+                height="240"
+                style={{
+                    marginWidth: 0 + "px",
+                    marginHeight: 0 + "px",
+                }}
+                scrolling="no"
+                frameBorder="0"
+                src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=2048b02-20&marketplace=amazon&amp;region=US&placement=B00U26V4VQ&asins=B00U26V4VQ&linkId=0fb00ea090251699f5675fab6a226a0a&show_border=false&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=E2D8D0"
+            ></iframe>
+        </div>
     );
 };
 
@@ -59,6 +87,19 @@ function updateGA() {
     ReactGA.initialize("UA-218917298-1");
     ReactGA.set({ page: pathName });
     ReactGA.pageview(pathName);
+}
+
+function update_layout() {
+    let w = window.innerWidth,
+        h = window.innerHeight;
+
+    const leftAds = document.getElementById("amazon-left-ads");
+    leftAds.style.marginLeft = "0px";
+    leftAds.style.marginTop = Math.round((h - 240) / 2) + "px";
+
+    const rightAds = document.getElementById("amazon-right-ads");
+    rightAds.style.marginLeft = Math.round(w - 120) + "px";
+    rightAds.style.marginTop = Math.round((h - 240) / 2) + "px";
 }
 
 export default Profile;
